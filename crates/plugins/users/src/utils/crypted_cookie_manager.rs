4442031080476255f5d89c285a9c45994ff81814
Paul Sajna <hello@paulsajna.com>
2023-01-14 21:17:57 -0800

    update test

--- crates/plugins/users/src/utils/crypted_cookie_manager.rs
+++ crates/plugins/users/src/utils/crypted_cookie_manager.rs
@@ -60,14 +60,16 @@ impl<'manager> CryptedCookieManager<'manager> {
 }
 
 #[test]
-fn test_existing_session_cookie() {
-    use std::collections::BTreeMap;
+fn test_develop_session_cookie() {
+    let cookie = "eyJhbGciOiJIUzI1NiJ9.eyJjb29raWVfdmVyc2lvbiI6IjIiLCJzZXNzaW9uX3Rva2VuIjoiU0VTU0lPTjpwanlrZXh2aHY3NHNxamIxZjJiNzBmMXoiLCJ1c2VyX3Rva2VuIjoiVTozQ01GNzZRSjM3RjI1In0.8xXVSKUna0x_KaJ4NJUamQvyX3GYcBjwNf7UD7Ix-hk";
+    let key = "notsecret";
+    let ccm = CryptedCookieManager::new("api.jungle.horse", &key);
+    let decrypted = ccm.decrypt_map(cookie).unwrap();
+    let mut expected_map: BTreeMap<String, String> = BTreeMap::new();
 
-    let cookie = "";
-    let key = "";
+    expected_map.insert("cookie_version".to_string(), "2".to_string());
+    expected_map.insert("session_token".to_string(), "SESSION:pjykexvhv74sqjb1f2b70f1z".to_string());
+    expected_map.insert("user_token".to_string(), "U:3CMF76QJ37F25".to_string());
 
-    let ccm = CryptedCookieManager::new("fakeyou.com", &key);
-
-    let decrypted = ccm.decrypt_map(existing_cookie).unwrap();
-    println!("{:#?}", decrypted);
+    assert_eq!(decrypted, expected_map);
 }
