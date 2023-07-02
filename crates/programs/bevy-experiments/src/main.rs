9cfa3bde0d332e8c8b790a8456f87e475483f59a
Paul Sajna <hello@paulsajna.com>
2023-07-02 13:51:50 -0700

    hacky spherical skyboxes

--- crates/programs/bevy-experiments/src/main.rs
+++ crates/programs/bevy-experiments/src/main.rs
@@ -1,8 +1,8 @@
 use bevy::{
-    prelude::*,
+    prelude::*, pbr::{NotShadowCaster, NotShadowReceiver},
 };
 
-//use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 mod gizmo;
 mod skybox;
@@ -26,14 +26,14 @@ fn main() {
         .add_plugins(DefaultPickingPlugins.build().disable::<DebugPickingPlugin>().disable::<DefaultHighlightingPlugin>())
         .add_plugin(TransformGizmoPlugin::default())
         .add_plugin(MaterialPlugin::<CubemapMaterial>::default())
-        //.add_plugin(WorldInspectorPlugin::new())
+        .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(setup)
         .add_startup_system(light_setup)
-        .add_startup_system(skybox_setup)
+//        .add_startup_system(skybox_setup)
         .add_startup_system(camera_setup)
         .add_startup_system(animation_setup)
         .add_system(gizmo_system)
-        .add_system(skybox_system)
+//        .add_system(skybox_system)
         .add_system(animation_system)
         .add_system(make_pickable)
         .add_event::<SelectRoot>()
@@ -44,7 +44,7 @@ fn main() {
 #[derive(Component)]
 pub(crate) struct Roko;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut materials: ResMut<Assets<StandardMaterial>>,mut commands: Commands, asset_server: Res<AssetServer>) {
     //roko scene
     let scene = asset_server.load("Roko_nogamerig.glb#Scene0");
     commands.spawn((SceneBundle {
@@ -53,6 +53,24 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     },
         Roko,
     ));
+    let spheremap_tex = asset_server.load("spheremap2.png");
+    let material = materials.add(StandardMaterial {
+        //base_color: Color::BLUE,
+        base_color_texture: Some(spheremap_tex.clone()),
+        emissive_texture: Some(spheremap_tex),
+        unlit: true,
+        ..default()
+    });
+    let sphere = asset_server.load("Sky_Sphere.glb#Mesh0/Primitive0");
+    commands.spawn((MaterialMeshBundle::<StandardMaterial> {
+        mesh: sphere,
+        material: material,
+        transform: Transform {
+            scale: Vec3::new(0.2, 0.2, 0.2),
+            ..default()
+        },
+        ..default()
+    }, NotShadowCaster, NotShadowReceiver));
 }
 
 
