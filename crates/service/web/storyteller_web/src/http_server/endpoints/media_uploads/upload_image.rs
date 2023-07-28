a52401176eb19f35842aae31348aaac2fd49d256
Brandon Thomas <echelon@gmail.com>
2023-07-28 01:10:29 -0400

    upload images (untested)

---
+++ crates/service/web/storyteller_web/src/http_server/endpoints/media_uploads/upload_image.rs
@@ -0,0 +1,50 @@
+use actix_multipart::Multipart;
+use actix_web::{HttpRequest, HttpResponse, web};
+use crate::http_server::endpoints::media_uploads::common::handle_upload::handle_upload;
+use crate::http_server::endpoints::media_uploads::common::upload_error::UploadError;
+use crate::server_state::ServerState;
+use once_cell::sync::Lazy;
+use std::collections::HashSet;
+use std::sync::Arc;
+use tokens::files::media_upload::MediaUploadToken;
+
+#[derive(Serialize)]
+pub struct UploadAudioSuccessResponse {
+  pub success: bool,
+  pub upload_token: MediaUploadToken,
+}
+
+static ALLOWED_MIME_TYPES : Lazy<HashSet<&'static str>> = Lazy::new(|| {
+  HashSet::from([
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+  ])
+});
+
+pub async fn upload_image_handler(
+  http_request: HttpRequest,
+  server_state: web::Data<Arc<ServerState>>,
+  mut multipart_payload: Multipart,
+) -> Result<HttpResponse, UploadError> {
+
+  let response = handle_upload(
+    &http_request,
+    &server_state,
+    multipart_payload,
+    &ALLOWED_MIME_TYPES).await?;
+
+  let media_upload_token = response.to_media_token();
+
+  let response = UploadAudioSuccessResponse {
+    success: true,
+    upload_token: media_upload_token,
+  };
+
+  let body = serde_json::to_string(&response)
+      .map_err(|e| UploadError::ServerError)?;
+
+  return Ok(HttpResponse::Ok()
+      .content_type("application/json")
+      .body(body));
+}
