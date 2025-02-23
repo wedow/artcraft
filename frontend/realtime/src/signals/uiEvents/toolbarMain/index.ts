67a5ea44c111402d2287ed8af86bf644e1bbd87e
Tanish Manku <tanish2k09.dev@gmail.com>
2025-02-23 02:51:41 -0800

    Add paint color signal events

--- frontend/realtime/src/signals/uiEvents/toolbarMain/index.ts
+++ frontend/realtime/src/signals/uiEvents/toolbarMain/index.ts
@@ -3,11 +3,14 @@ import {
   loadingBarRetryDispatch,
   loadingBarRetryEventHandler,
 } from "./loadingBar";
+import { onPaintColorChanged, setPaintColor } from "./paintMode";
 export const dispatchers = {
   ...buttonDispatchers,
   loadingBarRetry: loadingBarRetryDispatch,
+  setPaintColor,
 };
 export const eventsHandlers = {
   ...buttonEventsHandlers,
   loadingBarRetry: loadingBarRetryEventHandler,
+  onPaintColorChanged
 };
