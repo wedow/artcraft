d5d9fa5bc9ce4e9e2ad18e26e9110f39ac153413
bflatastic <bombayapirat@gmail.com>
2025-02-27 23:48:10 +0700

    added loading bar (dev only)

--- frontend/realtime/src/components/ui/LoadingBar.tsx
+++ frontend/realtime/src/components/ui/LoadingBar.tsx
@@ -35,17 +35,14 @@ export const LoadingBar = ({
     >
       <div
         className={twMerge(
-          "h-2.5 w-full rounded-full",
+          "h-1.5 w-full rounded-full",
           status && status === LoadingBarStatus.IDLE
-            ? "border-1 bg-gray-50 ring-1 ring-inset ring-gray-200"
-            : "bg-gray-200",
+            ? "bg-gray-50"
+            : "pointer-events-none border-0 bg-gray-600",
         )}
       >
         <div
-          className={twMerge(
-            "h-2.5 rounded-full bg-primary-500",
-            status === LoadingBarStatus.LOADING && "animate-pulse",
-          )}
+          className={twMerge("h-full rounded-full bg-primary/80")}
           style={{ width: `${progress}%` }}
         />
       </div>
