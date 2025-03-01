46ca51b46052f8677526e9f7c103ad35694e951d
bflatastic <bombayapirat@gmail.com>
2025-03-01 18:06:09 +0700

    loading indicator signals

--- frontend/realtime/src/components/features/ToolbarTopCenter/index.tsx
+++ frontend/realtime/src/components/features/ToolbarTopCenter/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "~/components/ui/Button";
 import { useState } from "react";
 import { BaseDialog } from "~/components/ui/BaseDialog";
 import { DialogTitle } from "@headlessui/react";
-import { LoadingBar, LoadingBarStatus } from "~/components/ui";
+import { SignaledLoadingIndicator } from "~/KonvaRootComponent/SignaledLoadingIndicator";
 
 interface ModelButtonProps {
   iconSrc: string;
@@ -88,38 +88,7 @@ export const ToolbarTopCenter = () => {
       )}
 
       {/* Loading models indicator (only shown in dev mode for now) */}
-      {import.meta.env.DEV && (
-        <div className="flex min-w-[328px] flex-col items-center">
-          <LoadingBar progress={70} status={LoadingBarStatus.LOADING} />
-          <div className="glass flex w-fit items-center gap-1.5 rounded-md px-2 py-1 text-xs font-medium opacity-60">
-            <svg
-              className="h-3 w-3 animate-spin text-white/40"
-              viewBox="0 0 64 64"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-            >
-              <path
-                d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z"
-                stroke="currentColor"
-                stroke-width="5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></path>
-              <path
-                d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
-                stroke="currentColor"
-                stroke-width="5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="text-white/90"
-              ></path>
-            </svg>
-            Downloading models - 70%
-          </div>
-        </div>
-      )}
+      {import.meta.env.DEV && <SignaledLoadingIndicator />}
     </div>
   );
 };
---
+++ frontend/realtime/src/signals/uiEvents/loadingIndicator.ts
@@ -0,0 +1,40 @@
+import { effect, signal } from "@preact/signals-react";
+
+// Model loading settings
+// - Is loading visible
+// - Loading progress percentage
+export const DEFAULT_IS_LOADING_VISIBLE = false;
+export const isLoadingVisible = signal<boolean>(DEFAULT_IS_LOADING_VISIBLE);
+const setIsLoadingVisible = (visible: boolean) => {
+  isLoadingVisible.value = visible;
+};
+
+const onIsLoadingVisibleChanged = (callback: (visible: boolean) => void) => {
+  effect(() => {
+    callback(isLoadingVisible.value);
+  });
+};
+
+export const DEFAULT_LOADING_PROGRESS = 0;
+export const loadingProgress = signal<number>(DEFAULT_LOADING_PROGRESS);
+const setLoadingProgress = (progress: number) => {
+  // Round to integer, then clamp between 0 and 100
+  loadingProgress.value = Math.max(0, Math.min(100, Math.round(progress)));
+};
+
+const onLoadingProgressChanged = (callback: (progress: number) => void) => {
+  effect(() => {
+    callback(loadingProgress.value);
+  });
+};
+
+// EXPORTS
+export const dispatchers = {
+  setIsLoadingVisible,
+  setLoadingProgress,
+};
+
+export const events = {
+  onIsLoadingVisibleChanged,
+  onLoadingProgressChanged,
+};
