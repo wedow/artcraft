d242c35a454c15f02a138d2ada4457be6585f5d6
Tanish Manku <tanish2k09.dev@gmail.com>
2025-03-25 23:08:48 -0700

    Update Edit mode UI

--- frontend/realtime/src/EditModeRootComponent/EditModeRootComponent.tsx
+++ frontend/realtime/src/EditModeRootComponent/EditModeRootComponent.tsx
@@ -1,10 +1,22 @@
-import { faSparkles } from "@fortawesome/pro-solid-svg-icons";
-import { useCallback, useRef, useState } from "react";
+import { faArrowPointer, faEdit, faHandPointer, faLocationArrow, faLocationArrowUp, faMousePointer, faNavicon, faSparkles } from "@fortawesome/pro-solid-svg-icons";
+import { useSignals } from "@preact/signals-react/runtime";
+import { useCallback, useRef } from "react";
 import { twMerge } from "tailwind-merge";
-import { Button } from "~/components/ui";
+import { DialogAddImage } from "~/components/features";
+import { Button, TabItem, TabSelector } from "~/components/ui";
 import { useRenderCounter } from "~/hooks/useRenderCounter";
 import { EditEngine } from "~/KonvaApp/EditEngine";
 import { KonvaCanvasContainer } from "~/KonvaRootComponent/KonvaCanvasContainer";
+import { EditMode, editModeState, setEditModeBaseImage } from "~/signals/editMode";
+
+const tabs: TabItem[] = [
+  { id: EditMode.SELECT, label: "Select", icon: faLocationArrow, iconClassName: "fa-flip-horizontal mr-1.5" },
+  { id: EditMode.EDIT, label: "Edit Region", icon: faEdit },
+];
+
+const setEditMode = (tabId: string) => {
+  editModeState.value = tabId as EditMode;
+}
 
 export const EditModeRootComponent = ({ className }: { className: string }) => {
   useRenderCounter("EditModeRootComponent");
@@ -19,44 +31,49 @@ export const EditModeRootComponent = ({ className }: { className: string }) => {
 
   const inputRef = useRef<HTMLTextAreaElement>(null);
 
+  useSignals();
+  const editMode = editModeState.value;
+
   return (
     <>
       <KonvaCanvasContainer
         className={className}
         ref={konvaContainerCallbackRef}
       />
-      <div className="fixed bottom-0 left-0 right-0 z-10 flex max-w-full items-center justify-center">
-        <div
-          className={twMerge(
-            "glass absolute bottom-12 flex min-h-[56px] w-full max-w-[900px] items-end rounded-xl border-2 p-3 shadow-xl transition-all duration-[400ms] ease-in-out",
-            "focused:border-primary-400/6 border-ui-panel",
-          )}
-        >
-          <textarea
-            ref={inputRef}
-            rows={1}
-            placeholder="Describe what you want to see..."
-            className="max-h-[120px] min-h-[40px] flex-1 resize-none overflow-y-auto rounded-md bg-transparent px-3 py-2 text-lg leading-normal focus:outline-none"
-            style={{ lineHeight: "24px" }}
-            onInput={(e) => {
-              const target = e.target as HTMLTextAreaElement;
-              target.style.height = "auto";
-              target.style.height = `${target.scrollHeight}px`;
-            }}
-            autoFocus={true}
+      {editMode === EditMode.INIT ?
+        (
+          <DialogAddImage
+            isOpen={true}
+            onAddImage={setEditModeBaseImage}
+            cancellable={false}
           />
-          <Button
-            icon={faSparkles}
-            variant="primary"
-            className="text-md ml-2 self-end"
-            onClick={() => {}}
-            disabled={false}
-            loading={false}
-          >
-            Generate
-          </Button>
-        </div>
-      </div>
+        )
+        :
+        (
+          <div className="fixed bottom-0 left-0 right-0 z-10 flex max-w-full items-center justify-center">
+            <div
+              className={twMerge(
+                "absolute bottom-12 flex min-h-[56px] items-end rounded-xl p-3 transition-all duration-[400ms] ease-in-out",
+              )}
+            >
+              <TabSelector
+                tabs={tabs}
+                activeTab={editMode}
+                onTabChange={(tabId) => { setEditMode(tabId) }}
+              />
+              <Button
+                icon={faSparkles}
+                variant="primary"
+                className="text-md ml-2 self-end"
+                onClick={() => { }}
+                disabled={false}
+                loading={false}
+              >
+                Generate
+              </Button>
+            </div>
+          </div >
+        )}
     </>
   );
 };
