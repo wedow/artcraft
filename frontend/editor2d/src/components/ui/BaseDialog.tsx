2516534b3fd1e106cd304d7f0fec3f8675c7a030
bflatastic <bombayapirat@gmail.com>
2025-04-09 01:36:36 +0700

    modal backdrop darker

--- frontend/editor2d/src/components/ui/BaseDialog.tsx
+++ frontend/editor2d/src/components/ui/BaseDialog.tsx
@@ -33,7 +33,7 @@ export const BaseDialog = ({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+          <div className="fixed inset-0 bg-black/60" aria-hidden="true" />
         </TransitionChild>
 
         <div className="fixed inset-0 flex items-center justify-center p-4">
