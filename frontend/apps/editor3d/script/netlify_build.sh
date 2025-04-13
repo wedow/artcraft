17a02041310e4bd98fc33c24cdb62065228f2a7f
Brandon Thomas <echelon@gmail.com>
2025-04-13 18:48:34 -0400

    netlify build test

--- frontend/apps/editor3d/script/netlify_build.sh
+++ frontend/apps/editor3d/script/netlify_build.sh
@@ -26,18 +26,27 @@ set -euxo pipefail
 echo "Run build script (TODO: Make strict)"
 nx build editor3d
 
+echo "Current dir (1)"
+pwd
+
+echo "List directory files (1)"
+pwd
+ls -lA .
+
 echo "Find build manifest"
 find . | grep manifest.json
 
 echo "Change to project dir"
 pushd apps/editor3d/
 
-echo "List directory files"
+echo "Current dir (2)"
+pwd
+
+echo "List directory files (2)"
 pwd
 ls -lA .
 
 echo "List build directory files"
-pwd
 ls -lA build/
 
 #echo "Copy netlify configs into dist"
