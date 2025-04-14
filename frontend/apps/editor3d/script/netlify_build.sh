8e120e02de9e3c35924ef1f5c2aa1def5fdd9d93
Brandon Thomas <echelon@gmail.com>
2025-04-13 20:23:38 -0400

    test

--- frontend/apps/editor3d/script/netlify_build.sh
+++ frontend/apps/editor3d/script/netlify_build.sh
@@ -74,7 +74,7 @@ echo "Copy files"
 cp -r build/server/* dist/
 
 #echo "Copy netlify configs into dist"
-#cp _headers dist/
+cp _headers dist/
 #cp _redirects dist/
 
 #echo "Copy netlify 404.html page into dist"
