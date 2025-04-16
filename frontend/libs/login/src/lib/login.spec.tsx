3d1777be2681adce9b3df5c729dd34ab0310f778
Brandon Thomas <echelon@gmail.com>
2025-04-16 01:09:05 -0400

    add login lib to test

---
+++ frontend/libs/login/src/lib/login.spec.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+
+import Login from './login';
+
+describe('Login', () => {
+  
+  it('should render successfully', () => {
+    const { baseElement } = render(<Login />);
+    expect(baseElement).toBeTruthy();
+  });
+  
+});
---
+++ frontend/libs/login/src/lib/login.tsx
@@ -0,0 +1,15 @@
+
+import styles from './login.module.css';
+
+export function Login() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Login!</h1>
+    </div>
+  );
+}
+
+export default Login;
+
+
+
