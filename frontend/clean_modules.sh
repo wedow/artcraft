837fc8a7b13fd22dc1ed6a1e9db366598cf718af
Brandon Thomas <echelon@gmail.com>
2025-04-16 17:26:43 -0400

    add more commands to clean_modules.sh

--- frontend/clean_modules.sh
+++ frontend/clean_modules.sh
@@ -2,8 +2,15 @@
 
 set -euxo pipefail
 
+echo "NX reset..."
+npx nx reset
+
 echo "Removing Node Modules..."
 rm -rf ./node_modules/
 rm -rf ./apps/editor2d/node_modules/
 rm -rf ./apps/editor3d/node_modules/
+
+echo "Removing Build Directories..."
+rm -rf ./libs/login/dist/
+rm -rf ./libs/api/dist/
 
