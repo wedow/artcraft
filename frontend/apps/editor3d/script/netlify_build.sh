6907b38a50df5077af5ccd6982af391cb1323d9a
Brandon Thomas <echelon@gmail.com>
2025-04-19 23:59:51 -0400

    write sha to build

--- frontend/apps/editor3d/script/netlify_build.sh
+++ frontend/apps/editor3d/script/netlify_build.sh
@@ -2,6 +2,17 @@
 
 set -euxo pipefail 
 
+# Add the GIT SHA to the build
+# This must be done before everything else, or it will get cached with the build.
+# (This might be making the builds less performant?)
+# COMMIT_REF is defined by Netlify to be the commit SHA
+# We want a short 8 character reference.
+echo "Labelling build with short SHA..."
+SHORT_SHA=$(echo "${COMMIT_REF}" | cut -c1-8)
+
+echo "Baking current git SHA to code";
+find . -type f -exec sed -i "s/%CURRENT_STORYTELLER_GIT_VERSION%/${SHORT_SHA}/g" {} +
+
 echo "Install packages"
 npm install
 
@@ -11,26 +22,10 @@ nx build editor3d --verbose
 echo "Change to project dir"
 pushd apps/editor3d/
 
-#echo "List build directory files"
-#ls -lA build/
-
-#echo "Make output directory"
-#mkdir dist/
-
-echo "DEBUGGING CURRENT STATE (1)"
-pwd
-ls -lA
-echo "DEBUGGING CURRENT STATE (2)"
-ls -lA src/
-
 echo "Copy files to putput directory"
-#cp -r build/server/* dist/
-#cp -r build/client/* dist/
 cp _headers dist/
 #cp _redirects dist/
 #cp "404.html" dist/
 
 echo "List final files in build"
 find dist/
-
-
