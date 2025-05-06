82ed0c5da36875c730ce7fcebfdf415052928efa
bflatastic <bombayapirat@gmail.com>
2025-05-06 10:26:23 +0700

    shared login modal component init

---
+++ frontend/libs/components/login-modal/README.md
@@ -0,0 +1,7 @@
+# @storyteller/ui-login-modal
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @storyteller/ui-login-modal` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ frontend/libs/components/login-modal/src/lib/login-modal.tsx
@@ -0,0 +1,15 @@
+
+import styles from './login-modal.module.css';
+
+export function LoginModal() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to LoginModal!</h1>
+    </div>
+  );
+}
+
+export default LoginModal;
+
+
+
