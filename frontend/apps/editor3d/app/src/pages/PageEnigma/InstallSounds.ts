9f0199c582c05f1029c56bdfbd465917087eee4a
Brandon Thomas <echelon@gmail.com>
2025-05-07 22:45:43 -0400

    lower volume

--- frontend/apps/editor3d/app/src/pages/PageEnigma/InstallSounds.ts
+++ frontend/apps/editor3d/app/src/pages/PageEnigma/InstallSounds.ts
@@ -6,10 +6,10 @@ export const InstallSounds = () => {
   if (r.hasSound("crumble")) {
     return;
   }
-  r.setSoundOnce("flower", new SoundEffect("resources/sound/smrpg_flower.wav"));
-  r.setSoundOnce("correct", new SoundEffect("resources/sound/smrpg_correct.wav"));
-  r.setSoundOnce("next", new SoundEffect("resources/sound/oot_dialogue_next.wav", { defaultVolume: 1.0 }));
-  r.setSoundOnce("done", new SoundEffect("resources/sound/oot_dialogue_done.wav", { defaultVolume: 1.0 }));
-  r.setSoundOnce("crumble", new SoundEffect("resources/sound/smrpg_drybones_crumble.wav", { defaultVolume: 0.5 }));
+  r.setSoundOnce("flower", new SoundEffect("resources/sound/smrpg_flower.wav", { defaultVolume: 0.1 }));
+  r.setSoundOnce("correct", new SoundEffect("resources/sound/smrpg_correct.wav", { defaultVolume: 0.3 }));
+  r.setSoundOnce("next", new SoundEffect("resources/sound/oot_dialogue_next.wav", { defaultVolume: 0.2 }));
+  r.setSoundOnce("done", new SoundEffect("resources/sound/oot_dialogue_done.wav", { defaultVolume: 0.2 }));
+  r.setSoundOnce("crumble", new SoundEffect("resources/sound/smrpg_drybones_crumble.wav", { defaultVolume: 0.1 }));
   (window as any).sounds = r;
 }
