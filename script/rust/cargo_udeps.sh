ba86033e6a9b9b590429c789968b30e7c56f433b
Brandon Thomas <echelon@gmail.com>
2025-05-26 00:29:41 -0400

    cleanup deps

--- script/rust/cargo_udeps.sh
+++ script/rust/cargo_udeps.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 
 echo 'Looking for unused dependencies'
 
+# NB(bt,2025-05-26): Previous version was "cargo-udeps 0.1.48"
 # https://github.com/est31/cargo-udeps
 # https://stackoverflow.com/a/72082679
 # Needs nightly to run
