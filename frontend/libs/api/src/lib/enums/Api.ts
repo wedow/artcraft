7ff2bacc3387ed817601a3c607290ed030dad1d4
Brandon Thomas <echelon@gmail.com>
2025-06-19 20:36:44 -0400

    localhost testing (temp)

--- frontend/libs/api/src/lib/enums/Api.ts
+++ frontend/libs/api/src/lib/enums/Api.ts
@@ -1,4 +1,6 @@
 export enum API_TARGETS {
+  // NB: Comment this out for quick testing in development
+  //BASE_API = "http://localhost:12345",
   BASE_API = "https://api.storyteller.ai",
   GOOGLE_API = "https://storage.googleapis.com",
   FUNNEL_API = "https://style.storyteller.ai",
