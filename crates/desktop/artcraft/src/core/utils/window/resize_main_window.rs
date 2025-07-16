16c14a49b430d12ab649ad3bceff5cefb3c5ca10
Brandon Thomas <echelon@gmail.com>
2025-07-16 19:01:44 -0400

    don't set window off to the bottom right

--- crates/desktop/artcraft/src/core/utils/window/resize_main_window.rs
+++ crates/desktop/artcraft/src/core/utils/window/resize_main_window.rs
@@ -2,6 +2,9 @@ use crate::core::state::window::main_window_size::MainWindowSize;
 use crate::core::windows::main_window::constants::{MAIN_WINDOW_MIN_HEIGHT, MAIN_WINDOW_MIN_WIDTH, MAIN_WINDOW_NAME};
 use tauri::{AppHandle, Manager, PhysicalSize};
 
+/// Try to resize the main window at startup.
+/// If it doesn't meet certain sizing heuristics, abandon manual resize and let the OS
+/// decide how to size the app.
 pub fn resize_main_window(
   app: &AppHandle,
   size: &MainWindowSize,
@@ -12,12 +15,12 @@ pub fn resize_main_window(
   } else if size.height < MAIN_WINDOW_MIN_HEIGHT {
     return Err(anyhow::anyhow!("Height must be at least {}", MAIN_WINDOW_MIN_HEIGHT));
   }
-  
+
   let windows = app.windows();
   let window = windows.get(MAIN_WINDOW_NAME)
       .ok_or_else(|| anyhow::anyhow!("Main window not found"))?;
-  
+
   window.set_size(PhysicalSize::new(size.width, size.height))?;
-  
+
   Ok(())
 }
