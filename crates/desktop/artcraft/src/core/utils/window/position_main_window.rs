5e7f8ca8d10c7fbf49e600f14a28d6a8158cb1a6
Brandon Thomas <echelon@gmail.com>
2025-07-16 18:30:29 -0400

    don't position off-screen (top left)

--- crates/desktop/artcraft/src/core/utils/window/position_main_window.rs
+++ crates/desktop/artcraft/src/core/utils/window/position_main_window.rs
@@ -7,12 +7,20 @@ pub fn position_main_window(
   app: &AppHandle,
   position: &MainWindowPosition,
 ) -> AnyhowResult<()> {
-  
+
+  // NB: This non-negative position heuristic is good for MacBook (single screen).
+  // Not sure how this fares on Windows, Linux, or multiscreen setups.
+  if position.x < 0 {
+    return Err(anyhow::anyhow!("X position must be non-negative"));
+  } else if position.y < 0 {
+    return Err(anyhow::anyhow!("Y position must be non-negative"));
+  }
+
   let windows = app.windows();
   let window = windows.get(MAIN_WINDOW_NAME)
       .ok_or_else(|| anyhow::anyhow!("Main window not found"))?;
-  
+
   window.set_position(PhysicalPosition::new(position.x, position.y))?;
-  
+
   Ok(())
 }
