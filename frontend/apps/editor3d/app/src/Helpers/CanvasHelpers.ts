3fbb1ae98818fce90df694b321f850edff60cc3a
Tanish Manku <tanish2k09.dev@gmail.com>
2025-08-05 13:52:08 -0700

    Use Offscreen Canvas to normalize

--- frontend/apps/editor3d/app/src/Helpers/CanvasHelpers.ts
+++ frontend/apps/editor3d/app/src/Helpers/CanvasHelpers.ts
@@ -1,12 +1,12 @@
 
-export const normalizeCanvas = (canvas: HTMLCanvasElement, width: number, height: number): HTMLCanvasElement => {
-  const newCanvas = document.createElement("canvas");
+export const normalizeCanvas = (canvas: HTMLCanvasElement, width: number, height: number): OffscreenCanvas => {
+  const newCanvas = new OffscreenCanvas(width, height);
   newCanvas.width = width;
   newCanvas.height = height;
 
   const ctx = newCanvas.getContext("2d");
   if (!ctx) {
-    throw new Error("Failed to get canvas context");
+    throw new Error("Failed to get offscreen canvas context");
   }
 
   ctx.imageSmoothingEnabled = true;
