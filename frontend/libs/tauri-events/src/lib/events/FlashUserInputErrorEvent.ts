375ed76f3fc73e11f3640cdbc333461d2f24154f
Brandon Thomas <echelon@gmail.com>
2025-08-16 02:32:41 -0400

    forgotten library file

---
+++ frontend/libs/tauri-events/src/lib/events/FlashUserInputErrorEvent.ts
@@ -0,0 +1,34 @@
+import { listen, UnlistenFn } from '@tauri-apps/api/event';
+import { BasicEventWrapper } from '../common/BasicEventWrapper';
+import { useEffect } from 'react';
+
+const EVENT_NAME : string = 'flash_user_input_error_event';
+
+export interface FlashUserInputErrorEvent {
+  message: string,
+}
+
+export const useFlashUserInputErrorEvent = (asyncCallback: (event: FlashUserInputErrorEvent) => Promise<void>) => {
+  useEffect(() => {
+    let isUnmounted = false;
+    let unlisten: Promise<UnlistenFn>;
+
+    const setup = async () => {
+      unlisten = listen<BasicEventWrapper<FlashUserInputErrorEvent>>(EVENT_NAME, async (wrappedEvent) => {
+        await asyncCallback(wrappedEvent.payload.data);
+      });
+
+      if (isUnmounted) {
+        unlisten.then(f => f()); // Unsubscribe if unmounted early.
+      }
+    };
+
+    setup();
+    
+    return () => {
+      isUnmounted = true;
+      unlisten.then(f => f());
+    };
+
+  }, []);
+}
