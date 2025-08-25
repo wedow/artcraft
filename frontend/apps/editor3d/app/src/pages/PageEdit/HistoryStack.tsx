ef473df9e429fc6d6311e53098c29ec09cb81906
bflatastic <bombayapirat@gmail.com>
2025-08-25 14:36:01 +0700

    history side buttons styling

--- frontend/apps/editor3d/app/src/pages/PageEdit/HistoryStack.tsx
+++ frontend/apps/editor3d/app/src/pages/PageEdit/HistoryStack.tsx
@@ -1,5 +1,8 @@
 import { Button } from "@storyteller/ui-button";
-import { EditedImage, useImageEditCompleteEvent } from "@storyteller/tauri-events";
+import {
+  EditedImage,
+  useImageEditCompleteEvent,
+} from "@storyteller/tauri-events";
 import { faTrash } from "@fortawesome/pro-solid-svg-icons";
 import { useState } from "react";
 import { BaseSelectorImage } from "./BaseImageSelector";
@@ -16,17 +19,20 @@ interface HistoryStackProps {
 
 export const HistoryStack = ({
   onClear,
-  onImageSelect = () => { },
+  onImageSelect = () => {},
   startingBundles = [],
 }: HistoryStackProps) => {
-
-  const [imageBundles, setImageBundles] = useState<ImageBundle[]>(startingBundles);
+  const [imageBundles, setImageBundles] =
+    useState<ImageBundle[]>(startingBundles);
   useImageEditCompleteEvent(async (event) => {
     const newBundle: ImageBundle = {
-      images: event.edited_images.map((editedImage) => ({
-        url: editedImage.cdn_url,
-        mediaToken: editedImage.media_token,
-      } as BaseSelectorImage)),
+      images: event.edited_images.map(
+        (editedImage) =>
+          ({
+            url: editedImage.cdn_url,
+            mediaToken: editedImage.media_token,
+          }) as BaseSelectorImage,
+      ),
     };
 
     imageBundles.push(newBundle);
@@ -36,23 +42,36 @@ export const HistoryStack = ({
   const handleClear = () => {
     setImageBundles(startingBundles);
     onClear();
-  }
+  };
 
   return (
-    <div className="w-16 h-auto max-h-1/2 overflow-y-auto rounded-lg bg-white">
+    <div className="max-h-1/2 glass h-auto w-16 overflow-y-auto rounded-lg p-1">
       <div className="flex flex-col-reverse items-center justify-center">
-        <Button icon={faTrash} type="reset" onClick={handleClear} />
+        <Button
+          className="h-7 w-full rounded-md opacity-80"
+          iconClassName="h-3 w-3"
+          icon={faTrash}
+          type="reset"
+          onClick={handleClear}
+          variant="destructive"
+        />
         {imageBundles.map((bundle) => (
           <>
-            <hr className="border-none bg-red w-full rounded-md h-2" />
+            <hr className="my-2 h-0.5 w-3/4 rounded-md border-none bg-white/10" />
             {bundle.images.map((image, imgIndex) => (
-              <Button key={imgIndex} className="w-full h-24" onClick={() => { onImageSelect(image) }}>
+              <Button
+                key={imgIndex}
+                className="aspect-square h-full w-full border-2 p-0"
+                onClick={() => {
+                  onImageSelect(image);
+                }}
+              >
                 {/* TODO: Fix CORS issue here */}
                 <img
                   src={image.url + "?historystack+" + Math.random()}
                   alt=""
                   crossOrigin="anonymous"
-                  className="w-full h-full rounded-lg"
+                  className="h-full w-full rounded-lg object-cover"
                 />
               </Button>
             ))}
@@ -61,4 +80,4 @@ export const HistoryStack = ({
       </div>
     </div>
   );
-}
+};
