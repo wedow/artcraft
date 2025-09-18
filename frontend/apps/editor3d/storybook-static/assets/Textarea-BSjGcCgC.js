b793ed7d6f8d9f1ff5635d8198d3c8541cb055ba
Brandon Thomas <echelon@gmail.com>
2025-09-17 20:23:21 -0400

    remove storybook

--- frontend/apps/editor3d/storybook-static/assets/Textarea-BSjGcCgC.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-BnIj46N_.js";import{useMDXComponents as i}from"./index-DLsXyOuj.js";import{M as a,C as e}from"./index-s6jI7ZwD.js";import{T as m,D as s,W as p}from"./Textarea.stories-DM9PTDF_.js";import"./index-CsdIBAqE.js";import"./iframe-CPVGSZP9.js";import"../sb-preview/runtime.js";import"./index-5f0m1pej.js";import"./index-DYADbu9O.js";import"./index-CwJzlvYf.js";import"./index-DrFu-skq.js";import"./bundle-mjs-Du4_Do6r.js";import"./utilities-FHh19gKC.js";import"./TransitionDialogue-BWOtE-kZ.js";import"./index-BeLTwFsi.js";import"./index-MS7LKRHD.js";import"./decorator-BcxtDL0x.js";import"./v4-D8aEg3BZ.js";function n(o){const r={h1:"h1",p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(a,{of:m}),`
-`,t.jsx(r.h1,{id:"textarea-control",children:"Textarea control"}),`
-`,t.jsx(r.p,{children:"Here is the Textarea with placeholder"}),`
-`,t.jsx(e,{of:s}),`
-`,t.jsx(r.p,{children:"Here is the Textarea with label"}),`
-`,t.jsx(e,{of:p})]})}function _(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{_ as default};
