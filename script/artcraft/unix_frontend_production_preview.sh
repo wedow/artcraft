ad4027b8735f5c7855551f011ad260c6e54c86ae
Brandon Thomas <echelon@gmail.com>
2025-10-09 05:06:41 -0400

    config+script to debug production builds

---
+++ script/artcraft/unix_frontend_production_preview.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# This works on Linux and MacOS to launch the frontend dev server
+
+root_dir=$(pwd)
+frontend_path="${root_dir}/frontend"
+
+echo "Running Artcraft Frontend in Production Build + Preview Mode... (It won't hot reload!)"
+echo ""
+
+echo "You'll need to launch the Rust dev server as a second script!"
+echo ""
+
+# Kill any process running on port 5741, which will block startup
+if lsof -i tcp:5173 &>/dev/null; then
+  lsof -i tcp:5173 -t | xargs kill -9
+  echo "Killed process running on port 5173"
+else
+  echo "No process running on port 5173"
+fi
+
+pushd "${frontend_path}" || exit
+
+npm install --verbose
+
+export VITE_ENVIRONMENT_TYPE="production"
+
+nx production-preview editor3d
+
+popd || exit
