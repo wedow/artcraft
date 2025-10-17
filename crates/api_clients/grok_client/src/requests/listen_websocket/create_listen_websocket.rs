86780171566032fcbe5cba1fba98c4b897ec37f3
Brandon Thomas <echelon@gmail.com>
2025-10-17 12:57:40 -0400

    still wip

--- crates/api_clients/grok_client/src/requests/listen_websocket/create_listen_websocket.rs
+++ crates/api_clients/grok_client/src/requests/listen_websocket/create_listen_websocket.rs
@@ -13,6 +13,7 @@ use wreq::Client;
 use wreq_util::Emulation;
 
 const WEBSOCKET_URL: &str = "wss://grok.com/ws/imagine/listen";
+//const WEBSOCKET_URL: &str = "https://grok.com/ws/imagine/listen";
 
 pub struct CreateListenWebsocketArgs<'a> {
   pub cookies: &'a str,
@@ -24,10 +25,11 @@ pub async fn create_listen_websocket(args: CreateListenWebsocketArgs<'_>) -> Res
 
 
   let client = Client::builder()
-      //.emulation(Emulation::Chrome140)
+      .emulation(Emulation::Chrome140)
       //.http1_only() // NB: Not needed - websockets are sent over HTTP/1.1 without this configuration
       .cert_verification(false) // TODO: REMOVE THIS.
       .connection_verbose(true)
+      .cookie_store(true)
       .connect_timeout(Duration::from_secs(10))
       //.keylog()
       //.http1_options(Http1Options::builder().build())
@@ -35,10 +37,14 @@ pub async fn create_listen_websocket(args: CreateListenWebsocketArgs<'_>) -> Res
       .map_err(|err| GrokClientError::WreqClientError(err))?;
 
   let cookies = args.cookies.to_string();
+
+  let cookies = "_ga=GA1.1.1232202746.1760710013; i18nextLng=en; x-anonuserid=621371fd-a877-4245-900c-1aa8db6039f2; x-challenge=2CHUrpA5J8%2FW2%2F1H8glEE3WKIXTKrbU7mMARWLT5DHzp5MS6nUk1aTHt7gqzv1vCxfnIdHTdeq4ZLYwcBj1DL%2FBptSSVT9OKi9DQDsN%2BKk0Ur3jg1uS%2BuPtwYSNZ%2B6CtuLCrDZqlDh%2FfFTzYr2sQ9nT3R72HfOyS%2FFXw0qvCgoROCtZCiGY%3D; x-signature=qPh93ojC8uCXOUvq7t0SzcwsyXKOE%2BRXH6dH26oL8lxwRIabYha6eniSt329QvpjBEeUqlekeStaek44mDpEYg%3D%3D; sso=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzZXNzaW9uX2lkIjoiOGU3MDFiNzctOTdkNC00ZjM0LWExOTctOWFmMDU1MzY3NDAwIn0.-a6x0InxbGzfTVfUlrdzxskxCnvMDI8lC90z4wHeGIk; sso-rw=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzZXNzaW9uX2lkIjoiOGU3MDFiNzctOTdkNC00ZjM0LWExOTctOWFmMDU1MzY3NDAwIn0.-a6x0InxbGzfTVfUlrdzxskxCnvMDI8lC90z4wHeGIk; stblid=b3331fc1-45d7-466b-83df-67427c0b2367; mp_ea93da913ddb66b6372b89d97b1029ac_mixpanel=%7B%22distinct_id%22%3A%2285980643-ffab-4984-a3de-59a608c47d7f%22%2C%22%24device_id%22%3A%2279ce237a-a0f3-4913-bf4b-519ac8a98263%22%2C%22%24initial_referrer%22%3A%22%24direct%22%2C%22%24initial_referring_domain%22%3A%22%24direct%22%2C%22__mps%22%3A%7B%7D%2C%22__mpso%22%3A%7B%7D%2C%22__mpus%22%3A%7B%7D%2C%22__mpa%22%3A%7B%7D%2C%22__mpu%22%3A%7B%7D%2C%22__mpr%22%3A%5B%5D%2C%22__mpap%22%3A%5B%5D%2C%22%24user_id%22%3A%2285980643-ffab-4984-a3de-59a608c47d7f%22%7D; cf_clearance=HBGeoT0XFc4oFd083362oxL5_2kXlFuxjVdhdqbjmvc-1760719202-1.2.1.1-otB93onljmuJsNnVq00Zliw5S5OFQB4dsZ6Ahx690e_o6vUxj2SgeVtjc4JaiYl5yqY4pi9K9LAMH3xCkIMioqX6QXq3VFm6SAnnz5xiorw4ZAP.ChbkUwop4HQ6pKOKIqq68vvpCibOsgpateGNFqgEmWhpJDT1pKarqAU6OB.w7uj4eNDPVWdRjBaTxAna9ehazWIF77E7K4cbz878nyqDxj_dLcSlHSCsM.Le5a8; _ga_8FEWB057YH=GS2.1.s1760719202$o2$g0$t1760719202$j60$l0$h0".to_string();
   println!("Cookies: {}", cookies);
 
-  println!("Configuring client...");
-  info!("Configuring client...");
+  get_tasks(&client, &cookies).await?;
+
+  println!("Configuring client for websocket...");
+  info!("Configuring client for websocket...");
 
   let builder = client.websocket(WEBSOCKET_URL)
       .header("Host", "grok.com") // Chrome
@@ -51,7 +57,7 @@ pub async fn create_listen_websocket(args: CreateListenWebsocketArgs<'_>) -> Res
       .header("Sec-WebSocket-Version", "13") // Chrome
       .header("Accept-Encoding", "gzip, deflate, br, zstd") // Chrome
       .header("Accept-Language", "en-US,en;q=0.9") // Chrome
-      .header("Cookie", "cf_clearance=o10zUU.x20fFOkj6tPIzI73QFT.kG3rLwdtt5P9KNzI-1760710012-1.2.1.1-q0vgGudZ42zQLs1EEEdVDkqeJGaJxkL0m60w05izUQ4NVtFNawTmElVUNYS5gOXSRnCjQkTmcMu0bI4yw3gkf.0EyfR9fE3McXK9zjwnMDZLC5MDsYazzaMA87sU4seMjd3G8oKfdi.r2pZ2rUzkoLSAJ11.q7IpHUvWsk8zcpxm1hLg9LcvfX0c.Sbcf.8mwY_32NVpIZT_0rzdp71FJUOZGabSOK4isjC409u7GOg; _ga=GA1.1.1232202746.1760710013; i18nextLng=en; x-anonuserid=621371fd-a877-4245-900c-1aa8db6039f2; x-challenge=2CHUrpA5J8%2FW2%2F1H8glEE3WKIXTKrbU7mMARWLT5DHzp5MS6nUk1aTHt7gqzv1vCxfnIdHTdeq4ZLYwcBj1DL%2FBptSSVT9OKi9DQDsN%2BKk0Ur3jg1uS%2BuPtwYSNZ%2B6CtuLCrDZqlDh%2FfFTzYr2sQ9nT3R72HfOyS%2FFXw0qvCgoROCtZCiGY%3D; x-signature=qPh93ojC8uCXOUvq7t0SzcwsyXKOE%2BRXH6dH26oL8lxwRIabYha6eniSt329QvpjBEeUqlekeStaek44mDpEYg%3D%3D; sso=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzZXNzaW9uX2lkIjoiOGU3MDFiNzctOTdkNC00ZjM0LWExOTctOWFmMDU1MzY3NDAwIn0.-a6x0InxbGzfTVfUlrdzxskxCnvMDI8lC90z4wHeGIk; sso-rw=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzZXNzaW9uX2lkIjoiOGU3MDFiNzctOTdkNC00ZjM0LWExOTctOWFmMDU1MzY3NDAwIn0.-a6x0InxbGzfTVfUlrdzxskxCnvMDI8lC90z4wHeGIk; stblid=b3331fc1-45d7-466b-83df-67427c0b2367; mp_ea93da913ddb66b6372b89d97b1029ac_mixpanel=%7B%22distinct_id%22%3A%2285980643-ffab-4984-a3de-59a608c47d7f%22%2C%22%24device_id%22%3A%2279ce237a-a0f3-4913-bf4b-519ac8a98263%22%2C%22%24initial_referrer%22%3A%22%24direct%22%2C%22%24initial_referring_domain%22%3A%22%24direct%22%2C%22__mps%22%3A%7B%7D%2C%22__mpso%22%3A%7B%7D%2C%22__mpus%22%3A%7B%7D%2C%22__mpa%22%3A%7B%7D%2C%22__mpu%22%3A%7B%7D%2C%22__mpr%22%3A%5B%5D%2C%22__mpap%22%3A%5B%5D%2C%22%24user_id%22%3A%2285980643-ffab-4984-a3de-59a608c47d7f%22%7D; _ga_8FEWB057YH=GS2.1.s1760710013$o1$g1$t1760710085$j60$l0$h0")
+      .header("Cookie", cookies)
       .header("Sec-WebSocket-Key", "BhBXbFSG6/1xcZVq4ySxcg==") // TODO
       .header("Sec-WebSocket-Extensions", "permessage-deflate; client_max_window_bits"); // Chrome
       //.header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:144.0) Gecko/20100101 Firefox/144.0")
@@ -78,6 +84,36 @@ pub async fn create_listen_websocket(args: CreateListenWebsocketArgs<'_>) -> Res
   let status = response.status();
   println!("Status: {}", status);
 
+  Ok(())
+}
+
+async fn get_tasks(client: &Client, cookies_header: &str) -> Result<(), GrokError> {
+  info!("Getting page...");
+
+  let builder = client.get("https://grok.com/rest/tasks")
+      .header(ACCEPT, "*/*")
+      .header(USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:144.0) Gecko/20100101 Firefox/144.0")
+      .header(ACCEPT_LANGUAGE, "en-US,en;q=0.5")
+      .header(ACCEPT_ENCODING, "gzip, deflate, br, zstd")
+      .header(ORIGIN, "https://grok.com")
+      .header(COOKIE, cookies_header)
+      .header(PRAGMA, "no-cache")
+      .header(CACHE_CONTROL, "no-cache");
+
+  info!("Sending...");
+
+  let response = builder.send()
+      .await
+      .map_err(|err| GrokClientError::WreqClientError(err))?;
+
+  let status = response.status();
+  println!("Status: {}", status);
+
+  let body = response.text()
+      .await
+      .map_err(|err| GrokGenericApiError::WreqError(err))?;
+
+  println!("Body: {}", body);
   Ok(())
 }
 
