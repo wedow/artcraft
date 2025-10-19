958015d33f782f46e4e515b547c70a5e84971ce7
Brandon Thomas <echelon@gmail.com>
2025-10-18 22:15:12 -0400

    mitm proxy wip

--- crates/api_clients/grok_client/src/requests/listen_websocket/create_listen_websocket_new.rs
+++ crates/api_clients/grok_client/src/requests/listen_websocket/create_listen_websocket_new.rs
@@ -1,13 +1,13 @@
-use crate::client::browser_user_agents::FIREFOX_143_MAC_USER_AGENT;
 use crate::error::grok_client_error::GrokClientError;
 use crate::error::grok_error::GrokError;
 use crate::requests::listen_websocket::cookies::SESSION_COOKIES_WITHOUT_CF_CLEARANCE;
 use std::ops::Deref;
 use std::time::Duration;
 use tokio::io::AsyncReadExt;
-use wreq::header::USER_AGENT;
 use wreq::{Client, Proxy};
+use wreq::header::{ACCEPT_LANGUAGE, ORIGIN, USER_AGENT};
 use wreq_util::Emulation;
+use crate::client::browser_user_agents::FIREFOX_143_MAC_USER_AGENT;
 
 const WEBSOCKET_URL: &str = "wss://grok.com/ws/imagine/listen";
 
@@ -24,14 +24,13 @@ pub async fn create_listen_websocket_new() -> Result<(), GrokError> {
   Wreq Firefox is IDENTICAL to Real Firefox Normal Mode (non-private) mode. - So why is it not working!?
    */
 
-  let proxy = Proxy::https("http://127.0.0.1:8080")
-      .map_err(|err| GrokClientError::WreqClientError(err))?;
+  //let proxy = Proxy::https("http://127.0.0.1:8080").map_err(|err| GrokClientError::WreqClientError(err))?;
 
   let client = Client::builder()
       .emulation(Emulation::Firefox143)
       .connection_verbose(true)
-      .cert_verification(false)
-      .proxy(proxy)
+      //.cert_verification(false)
+      //.proxy(proxy)
       .connect_timeout(Duration::from_secs(10))
       .build()
       .map_err(|err| GrokClientError::WreqClientError(err))?;
@@ -40,10 +39,14 @@ pub async fn create_listen_websocket_new() -> Result<(), GrokError> {
   //.cookie_store(true)
 
   let builder = client.websocket(WEBSOCKET_URL)
-      //.header("Accept", "*/*") // NB: Accept isn't in Chrome MITMProxy
+      //.header("Host", "grok.com")
       .header("Cookie", SESSION_COOKIES_WITHOUT_CF_CLEARANCE)
-      .header("Host", "grok.com")
-      .header(USER_AGENT, FIREFOX_143_MAC_USER_AGENT);
+      .default_headers(false)
+      .header(ORIGIN,"https://grok.com")
+      .header(ACCEPT_LANGUAGE, "en-US,en;q=0.9")
+      .header(USER_AGENT, FIREFOX_143_MAC_USER_AGENT)
+      .header("Sec-WebSocket-Extensions", "permessage-deflate; client_max_window_bits");
+  //.header("Accept", "*/*") // NB: Accept isn't in Chrome MITMProxy
 
   let response = builder.send()
       .await
