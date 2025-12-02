bbb62bb1a8126b405a66bc72569652c5a811552d
Brandon Thomas <echelon@gmail.com>
2025-12-02 14:20:10 -0500

    fal.rs nanobanana wrappers

--- crates/vendor/fal-rs/src/endpoints/fal_ai/nano_banana_pro/nano_banana_pro_image_edit.rs
+++ crates/vendor/fal-rs/src/endpoints/fal_ai/nano_banana_pro/nano_banana_pro_image_edit.rs
@@ -33,37 +33,21 @@ pub struct NanoBananaProImageEditInput {
   ],
   "resolution": "1K"
 }
- */
+*/
 
 #[derive(Debug, Serialize, Deserialize, Default)]
-pub struct File {
+pub struct ImageFile {
   /// The URL where the file can be downloaded from.
   pub url: String,
-
-  // /// The mime type of the file.
-  // /// "image/png"
-  // #[serde(skip_serializing_if = "Option::is_none")]
-  // pub content_type: Option<String>,
-  // /// File data
-  // #[serde(skip_serializing_if = "Option::is_none")]
-  // pub file_data: Option<String>,
-  // /// The name of the file. It will be auto-generated if not provided.
-  // /// "z9RV14K95DvU.png"
-  // #[serde(skip_serializing_if = "Option::is_none")]
-  // pub file_name: Option<String>,
-  // /// The size of the file in bytes.
-  // /// 4404019
-  // #[serde(skip_serializing_if = "Option::is_none")]
-  // pub file_size: Option<i64>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Gemini25FlashImageEditOutput {
-  pub images: Vec<File>,
+pub struct NanoBananaProImageEditOutput {
+  pub images: Vec<ImageFile>,
 }
 
 pub fn gemini_25_flash_image_edit(
-  params: Gemini25FlashImageEditInput,
-) -> FalRequest<Gemini25FlashImageEditInput, Gemini25FlashImageEditOutput> {
-  FalRequest::new("fal-ai/gemini-25-flash-image/edit", params)
+  params: NanoBananaProImageEditInput,
+) -> FalRequest<NanoBananaProImageEditInput, NanoBananaProImageEditOutput> {
+  FalRequest::new("fal-ai/nano-banana-pro/edit", params)
 }
