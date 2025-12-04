e60d8f8a138a3fe69ed282de8e938433df0e584f
Brandon Thomas <echelon@gmail.com>
2025-12-04 15:58:09 -0500

    image to video fast

--- crates/api_clients/fal_client/src/requests/webhook/video/image/mod.rs
+++ crates/api_clients/fal_client/src/requests/webhook/video/image/mod.rs
@@ -6,6 +6,7 @@ pub mod enqueue_kling_v2p6_pro_image_to_video_webhook;
 pub mod enqueue_pixverse_v5_image_to_video_webhook;
 pub mod enqueue_sora_2_image_to_video_webhook;
 pub mod enqueue_sora_2_pro_image_to_video_webhook;
-pub mod enqueue_veo_3p1_first_last_frame_image_to_video_webhook;
 pub mod enqueue_veo_3p1_fast_first_last_frame_image_to_video_webhook;
+pub mod enqueue_veo_3p1_fast_image_to_video_webhook;
+pub mod enqueue_veo_3p1_first_last_frame_image_to_video_webhook;
 pub mod enqueue_veo_3p1_image_to_video_webhook;
