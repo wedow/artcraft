9d02ed35e7129164b6fac15d0145bdc2e0fae1d5
Brandon Thomas <echelon@gmail.com>
2025-12-31 13:08:06 -0500

    aspect ratio options from seedance

--- crates/desktop/artcraft/src/core/api_adapters/aspect_ratio/common_aspect_ratio.rs
+++ crates/desktop/artcraft/src/core/api_adapters/aspect_ratio/common_aspect_ratio.rs
@@ -8,22 +8,37 @@ use serde_derive::{Deserialize, Serialize};
 pub enum CommonAspectRatio {
   // Auto (eg. for image editing to use the source; used by Nano Banana Pro edit image, but not text-to-image)
   Auto,
+
   // Square
   Square,
+
   // Wide
   WideFiveByFour,
   WideFourByThree,
   WideThreeByTwo,
   WideSixteenByNine,
   WideTwentyOneByNine,
+
   // Tall
   TallFourByFive,
   TallThreeByFour,
   TallTwoByThree,
   TallNineBySixteen,
   TallNineByTwentyOne,
+
   // Imprecise semantic values that we probably remap to other meanings
   // on a model-by-model basis.
   Wide,
   Tall,
+
+  //Possible enum values: square_hd, square,
+  // portrait_4_3, portrait_16_9, landscape_4_3, landscape_16_9,
+  // auto, auto_2K, auto_4K
+
+  // Auto values that bake in resolution
+  Auto2k,
+  Auto4k,
+
+  // Defined aspect ratios that bake in resolution
+  SquareHd,
 }
