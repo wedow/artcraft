fbad200dee1bd571c570eed3e786e96170665734
Brandon Thomas <echelon@gmail.com>
2026-01-03 20:06:01 -0500

    fix soundregistry singleton issues from nx build

--- frontend/libs/soundboard/src/lib/SoundRegistry.ts
+++ frontend/libs/soundboard/src/lib/SoundRegistry.ts
@@ -1,8 +1,6 @@
 import { SoundEffect } from "./SoundEffect";
 
 export class SoundRegistry {
-  static #instance: SoundRegistry | undefined;
-
   #sounds: Map<string, SoundEffect>;
 
   private constructor() {
@@ -10,10 +8,30 @@ export class SoundRegistry {
   }
 
   public static getInstance() {
-    if (SoundRegistry.#instance === undefined) {
-      SoundRegistry.#instance = new SoundRegistry();
+    // NB(bt,2026-01-03): Our `nx` build setup is deeply flawed and in need 
+    // of repair: Production builds will compile libraries to multiple 
+    // independent symbolic spaces, making singleton pattern unreliable.
+    // Effectively, the singletons cannot be used across bundle boundaries.
+    // We're going to be gross and bind everything to the global namespace.
+    // 
+    // To be clear, this does not work:
+    //
+    //    class SoundRegistry {
+    //       static #instance: SoundRegistry | undefined;
+    //
+    //       [...]
+    //
+    //       static getInstance() {
+    //         if (SoundRegistry.#instance === undefined) {
+    //           SoundRegistry.#instance = new SoundRegistry();
+    //         }
+    //         return SoundRegistry.#instance;
+    //       }
+
+    if ((window as any).artcraft_sound_registry === undefined) {
+      (window as any).artcraft_sound_registry = new SoundRegistry();
     }
-    return SoundRegistry.#instance;
+    return (window as any).artcraft_sound_registry;
   }
 
   public hasSound(key: string) : boolean {
