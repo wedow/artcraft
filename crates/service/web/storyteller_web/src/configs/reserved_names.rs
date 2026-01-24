088a59510e44006ad536e4a89c9eb7c5568bd211
Brandon Thomas <echelon@gmail.com>
2026-01-24 09:09:46 -0500

    reorganize reserved usernames

--- crates/service/web/storyteller_web/src/configs/reserved_names.rs
+++ crates/service/web/storyteller_web/src/configs/reserved_names.rs
@@ -1,6 +1,6 @@
 
 /// Full string matches that can not be used as usernames.
-pub const RESERVED_USERNAMES : &str = include_str!("../../../../../../includes/binary_includes/reserved_usernames.txt");
+pub const RESERVED_USERNAMES : &str = include_str!("../../../../../../includes/binary_includes/usernames/reserved_usernames.txt");
 
 /// Substrings that are reserved and cannot be used as part of a username.
-pub const RESERVED_SUBSTRINGS : &str = include_str!("../../../../../../includes/binary_includes/reserved_usernames_including.txt");
+pub const RESERVED_SUBSTRINGS : &str = include_str!("../../../../../../includes/binary_includes/usernames/reserved_usernames_including.txt");
